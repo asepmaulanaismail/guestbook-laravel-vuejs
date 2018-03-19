a686e6742c1f1230d254fbbec7e8f249d70a839c
Asep Maulana Ismail <asepmaulanaismail@gmail.com>
2018-03-19 07:16:50 +0700

    Initial commit

---
+++ database/factories/SignatureFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
+$factory->define(App\Signature::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->safeEmail,
+        'body' => $faker->sentence
+    ];
+});
